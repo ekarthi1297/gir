8535b50af42e176b8e8cd301c2dcf4a88f2770d2
EC2 Default User <ec2-user@ip-172-31-86-175.ec2.internal>
2024-04-12 06:19:31 +0000

    Testing Code

---
+++ gir.java
@@ -0,0 +1,10 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+        System.out.println("Addition: " + num1 + " + " + num2 + " = " + sum);
+        int difference = num1 - num2;
+        System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + difference);
+    }
+}
